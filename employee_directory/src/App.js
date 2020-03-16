133b89791d0351ff434cd1821907cbf6ae02bfe5
kelz <kelsie.langston@yahoo.com>
2020-03-16 02:00:53 -0400

    partial search

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -3,28 +3,64 @@ import Jumbotron from './Components/Jumbotron/jumbotron';
 import Search from './Components/Search/search';
 import DirectoryHeader from './Components/DirectoryHeader/directoryHeader';
 import Employees from './Components/Employees/employees';
+import axios from 'axios';
 
 class App extends Component {
   state = {
-    sortBy: 'A-Z'
+    sortBy: "",
+    employees: {
+      results: []
+    },
+    search: ""
   }
 
   handleSortBy = (event) => {
     event.preventDefault();
-    if (this.state.sortBy === 'A-Z') {
+    if (this.state.sortBy === 'A-Z' || this.state.sortBy === "") {
       this.setState({ sortBy: 'Z-A' })
     } else {
       this.setState({ sortBy: 'A-Z' });
     }
   }
 
+  componentDidMount() {
+    axios.get('https://randomuser.me/api/?results=200&nat=us')
+      .then(res => {
+        //console.log(res.data)
+        this.setState({ employees: res.data })
+      })
+  }
+
+  handleFilter = searchingData => {
+    const newResults = this.state.employees.results.filter(val => {
+      let filterName = val.name.first.toUpperCase();
+      let searchName = searchingData.toUpperCase();
+      return filterName === searchName;
+    });
+    console.log(newResults)
+    console.log(this.state.employees)
+    if (newResults.length > 0) {
+      this.setState({ employees: { ...this.state.employees, results: newResults } })
+    } else {
+      //show all
+    }
+
+  }
+
+  handleSearch = event => {
+    event.preventDefault();
+    this.setState({ search: event.target.value });
+    this.handleFilter(event.target.value);
+  }
+
+
   render() {
     return (
       <div>
         <Jumbotron />
-        <Search />
+        <Search handleSearch={this.handleSearch} />
         <DirectoryHeader sort={this.handleSortBy} />
-        <Employees sortBy={this.state.sortBy} />
+        <Employees sortBy={this.state.sortBy} employees={this.state.employees} />
       </div>
     );
   }
--- employee_directory/src/Components/Search/search.js
+++ employee_directory/src/Components/Search/search.js
@@ -1,24 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Search extends Component {
-    state = {
-        search: ""
-    }
+const Search = props => {
 
-    handleSearch = event => {
-        event.preventDefault();
-        this.setState({ search: event.target.value })
-    }
-    
-    render(){
-        return (
-            <div className="w-100 d-flex justify-content-center py-4 bg-secondary">
-                <div className="w-25">
-                    <input onChange={this.handleSearch} type="text" className="form-control" placeholder="Search" aria-label="Search"></input>
-                </div>
+    return (
+        <div className="w-100 d-flex justify-content-center py-4 bg-secondary">
+            <div className="w-25">
+                <input onChange={props.handleSearch} type="text" className="form-control" placeholder="Search" aria-label="Search"></input>
             </div>
-        )
-    }
+        </div>
+    )
+    
 }
 
 export default Search;
