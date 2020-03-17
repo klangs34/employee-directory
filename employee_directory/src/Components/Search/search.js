9b0da369c16cd6195dfd401379066f1561da7579
kelz <kelsie.langston@yahoo.com>
2020-03-17 15:44:56 -0400

    change search placeholder name

--- employee_directory/src/Components/Search/search.js
+++ employee_directory/src/Components/Search/search.js
@@ -5,7 +5,7 @@ const Search = props => {
     return (
         <div className="w-100 d-flex justify-content-center py-4 bg-secondary">
             <div className="w-25">
-                <input onChange={props.handleSearch} type="text" className="form-control" placeholder="Search" aria-label="Search"></input>
+                <input onChange={props.handleSearch} type="text" className="form-control" placeholder="Search Name" aria-label="Search"></input>
             </div>
         </div>
     )
